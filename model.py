ad63ac4e210983d48e5a77507af812f13146fa9f
AdamPiechowiak <adampiechowiak16@gmail.com>
2023-05-23 14:41:01 +0200

    models predict one future week

--- model.py
+++ model.py
@@ -51,14 +51,13 @@ X_val, y_val = create_sequences(scaler.transform(val_data), seq_length)
 X_test, y_test = create_sequences(scaler.transform(test_data), seq_length)
 
 
-print(X_train.shape)
-print(y_train.shape)
-print(X_val.shape)
-print(y_val.shape)
-print(X_test.shape)
-print(y_test.shape)
+# print(X_train.shape)
+# print(y_train.shape)
+# print(X_val.shape)
+# print(y_val.shape)
+# print(X_test.shape)
+# print(y_test.shape)
 
-print(y_test[:,0])
 
 input_tensor = output_tensor = Input(X_train.shape[1:])
 output_tensor = TimeDistributed(Dense(32, activation = 'selu'))(output_tensor)
@@ -70,9 +69,9 @@ model = Model(inputs = input_tensor, outputs = output_tensor)
 model.compile(optimizer='RMSProp', loss='MeanSquaredError')
 
 plot_model(model)
-print(model.summary())
+# print(model.summary())
 
-print(y_train[:,0].shape)
+# print(y_train[:,0].shape)
 
 open_model = copy.deepcopy(model)
 high_model =  copy.deepcopy(model)
@@ -81,67 +80,62 @@ close_model = copy.deepcopy(model)
 adj_model = copy.deepcopy(model)
 volume_model = copy.deepcopy(model)
 
-open_model.fit(x = X_train, y = y_train[:,0], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,0]))
-high_model.fit(x = X_train, y = y_train[:,1], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,1]))
-low_model.fit(x = X_train, y = y_train[:,2], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,2]))
-close_model.fit(x = X_train, y = y_train[:,3], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,3]))
-adj_model.fit(x = X_train, y = y_train[:,4], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,4]))
-volume_model.fit(x = X_train, y = y_train[:,5], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,5]))
 
-'''
+acc_list = []
+model_list = [open_model,high_model,low_model,close_model,adj_model,volume_model]
 
-y_pred = open_model.predict(X_test)
+#trenowanie modeli i obliczenie dokladnosci
+for i in range(len(model_list)):
+    model_list[i].fit(x = X_train, y = y_train[:,i], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,i]))
 
-y_pred = scaler.inverse_transform(y_pred)
-y_test = scaler.inverse_transform(y_test)
+# open_model.fit(x = X_train, y = y_train[:,0], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,0]))
+# high_model.fit(x = X_train, y = y_train[:,1], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,1]))
+# low_model.fit(x = X_train, y = y_train[:,2], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,2]))
+# close_model.fit(x = X_train, y = y_train[:,3], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,3]))
+# adj_model.fit(x = X_train, y = y_train[:,4], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,4]))
+# volume_model.fit(x = X_train, y = y_train[:,5], batch_size=32, epochs=20, validation_data=(X_val, y_val[:,5]))
 
-x = np.arange(y_pred.shape[0])
-plt.figure(1)
-plt.subplot(211)
-plt.plot(x, y_pred, label = 'Predykcje')
-plt.plot(x, y_test[:,0], label = 'Wartości prawdziwe')
-plt.legend()
-diff = y_pred.squeeze()-y_test[:,0]
-plt.subplot(212)
-plt.plot(x, diff, label = 'różnice')
-plt.legend()
+    y_pred = model_list[i].predict(X_test)
+        
+    diff_pred = np.diff(y_pred.squeeze())
+    diff_true = np.diff(y_test[:,i])
+    diff_pred = np.sign(diff_pred)
+    diff_true = np.sign(diff_true)
+    #print(confusion_matrix(diff_true, diff_pred))
+    #print(accuracy_score(diff_true, diff_pred))
+        
+    acc_list.append(accuracy_score(diff_true, diff_pred))
 
-diff_pred = np.diff(y_pred.squeeze())
-diff_true = np.diff(y_test[:,0])
-diff_pred = np.sign(diff_pred)
-diff_true = np.sign(diff_true)
-print(confusion_matrix(diff_true, diff_pred))
-print(accuracy_score(diff_true, diff_pred))
-
-to_predict = scaler.transform(data[-101:-1])
-to_predict = to_predict.reshape((1,)+to_predict.shape)
-predict_o = open_model.predict(to_predict)
-predict_h = high_model.predict(to_predict)
-predict_l = low_model.predict(to_predict)
-predict_c = close_model.predict(to_predict)
-#predict_a = adj_model.predict(to_predict)
-#predict_v = volume_model.predict(to_predict)
-'''
+print(acc_list)
 
 
-to_predict = scaler.transform(data[-101:-1])
-to_predict = to_predict.reshape((1,)+to_predict.shape)
-predict_o = open_model.predict(to_predict)
-predict_h = high_model.predict(to_predict)
-predict_l = low_model.predict(to_predict)
-predict_c = close_model.predict(to_predict)
-predict_a = adj_model.predict(to_predict)
-predict_v = volume_model.predict(to_predict)
+#przewidywanie cen dla następnego tygodnia
+predict_data = np.array(data[:])
 
-p=np.array([predict_o,predict_h,predict_l,predict_c,predict_a,predict_v])
-print(p.shape)
+for i in range(5):
+    to_predict = scaler.transform(predict_data[-100:])
+    to_predict = to_predict.reshape((1,)+to_predict.shape)
+    predict_o = open_model.predict(to_predict)
+    predict_h = high_model.predict(to_predict)
+    predict_l = low_model.predict(to_predict)
+    predict_c = close_model.predict(to_predict)
+    predict_a = adj_model.predict(to_predict)
+    predict_v = volume_model.predict(to_predict)
+    
+    p=np.array([predict_o,predict_h,predict_l,predict_c,predict_a,predict_v])
+
+    p = p.reshape((1,6))
+
+    p2 = scaler.inverse_transform(p)
+    predict_data = np.append(predict_data,p2,axis=0)
+    
 
 
-p = p.reshape((1,6))
-
-p2 = scaler.inverse_transform(p)
-y_test2 = scaler.inverse_transform(y_test)
-
+for i in range(len(model_list)):
+    x = range(len(predict_data[-30:,i]))
+    plt.figure(i)
+    plt.plot(x, predict_data[-30:,i], label = 'Predykcja '+str(i))
+    plt.legend()
 '''
 y_pred = volume_model.predict(X_test)
 
