86a504015de7efc87607996d0ddc68e1a03f6d54
AdamPiechowiak <adampiechowiak16@gmail.com>
2023-05-24 23:41:41 +0200

    update data at 10AM everyday

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import streamlit as st
-import yfinance as yf
-import datetime
 import pandas as pd
-import numpy as np
-from keras.layers import GRU, Input, LSTM, Dropout
-from keras.models import Model
-from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.preprocessing import StandardScaler
-from keras.layers import TimeDistributed, Dense
-from keras.utils.vis_utils import plot_model
-import copy
 
 
 data = pd.read_csv("oil_price.csv",parse_dates=True,index_col="Date")
